3f9227c5c86c15aa8d05f31162324526c6557f6d
Teresa-CoCo <primeytklsb@gmail.com>
2024-07-31 20:06:40 +0800

    chore: Update .gitignore to include screenshot.png file and add button_function code in TeresaQT.py

--- TeresaQT.py
+++ TeresaQT.py
@@ -1,18 +1,31 @@
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QPlainTextEdit, QLCDNumber, QScrollBar, QMessageBox
-from PIL import ImageGrab
+import _thread as thread
 import base64
-import websocket
-import json
-import ssl
 import datetime
 import hashlib
 import hmac
-from urllib.parse import urlparse, urlencode
-from wsgiref.handlers import format_date_time
-import configparser
 import io
+import json
+import sys
+from urllib.parse import urlparse
+import ssl
+from datetime import datetime
+from time import mktime
+from urllib.parse import urlencode
+from wsgiref.handlers import format_date_time
+from PIL import ImageGrab
+import tkinter as tk
+from tkinter import messagebox
+import websocket  # 使用websocket_client
+import os
+import sys
+import configparser
+original_stdout = sys.stdout
+output = io.StringIO()
+sys.stdout = output
+
 
 # Form implementation generated from reading ui file 'dialog.ui'
 #
@@ -24,6 +37,26 @@ import io
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 
+def read_config(filename='config.ini'):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    
+    # 读取配置文件中的账号密码
+    appid = config.get('Credentials', 'appid')
+    api_secret = config.get('Credentials', 'api_secret')
+    api_key = config.get('Credentials', 'api_key')
+    
+    return appid, api_secret, api_key
+base_path = getattr(sys,'_MEIPASS',os.path.dirname(os.path.abspath(__file__)))
+configs = os.path.join(base_path, 'secret.ini')
+logo = os.path.join(base_path, 'logo.ico')
+appid, api_secret, api_key = read_config(configs)
+SPARKAI_APP_ID = appid
+SPARKAI_API_SECRET = api_secret
+SPARKAI_API_KEY = api_key
+
+imageunderstanding_url = "wss://spark-api.cn-huabei-1.xf-yun.com/v2.1/image"  # 识图云端环境的服务地址
+answer =" "
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -134,9 +167,9 @@ class Ui_Dialog(object):
         self.lcdNumber.setProperty("value", 1.0)
         self.lcdNumber.setProperty("intValue", 1)
         self.lcdNumber.setObjectName("lcdNumber")
-        self.pushButton.clicked.connect(self.ImageUnderstand)
+        self.pushButton_3.clicked.connect(self.screenshot_and_ask)
         self.pushButton_2.clicked.connect(self.open_life_tool)
-        # self.pushButton_3.clicked.connect(self.image_understand)
+        self.pushButton.clicked.connect(self.talktospark)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -156,7 +189,7 @@ class Ui_Dialog(object):
         except Exception as e:
             QMessageBox.critical(self, "截图失败", f"发生错误: {str(e)}")
 
-    def ImageUnderstand(url1):
+    def ImageUnderstand(self,url1):
         def imageUnderstanding(url):
             imagedata = open(url, 'rb').read()
 
@@ -327,6 +360,55 @@ class Ui_Dialog(object):
     def open_life_tool(self):
         from LifeTool.mainGUI import GUI
         GUI()
+    def screenshot_and_ask(self):
+        self.take_screenshot()
+        # exe_dir = os.path.dirname(sys.executable)
+        # file_path = os.path.join(exe_dir, 'screenshot.png')
+        output_text = self.ImageUnderstand("screenshot.png")
+        self.plainTextEdit_2.setPlainText(output_text + "\n")
+    def spark(self,input):
+        from sparkai.llm.llm import ChatSparkLLM, ChunkPrintHandler
+        from sparkai.core.messages import ChatMessage
+        import re
+        # 星火认知大模型Spark Max的URL值（https://www.xfyun.cn/doc/spark/Web.html）
+        SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
+        # 星火认知大模型Spark Max的domain值（https://www.xfyun.cn/doc/spark/Web.html）
+        SPARKAI_DOMAIN = 'generalv3.5'
+
+        if __name__ == '__main__':
+            spark = ChatSparkLLM(
+                spark_api_url=SPARKAI_URL,
+                spark_app_id=SPARKAI_APP_ID,
+                spark_api_key=SPARKAI_API_KEY,
+                spark_api_secret=SPARKAI_API_SECRET,
+                spark_llm_domain=SPARKAI_DOMAIN,
+                streaming=False,
+            )
+            messages = [ChatMessage(
+                role="user",
+                content=input
+            )]
+            handler = ChunkPrintHandler()
+            result = spark.generate([messages], callbacks=[handler])
+            a = str(result)
+            pattern = r"text='(.*?)'"
+            match = re.search(pattern, a)
+
+            if match:
+                extracted_text = match.group(1)
+                extracted_text = extracted_text.replace('\\n', '\n')
+
+                rresult = extracted_text
+                return rresult
+                print(extracted_text)
+            else:
+                print("Error 1")
+    def talktospark(self):
+        input_text = self.plainTextEdit.toPlainText()
+        output_text = self.spark(input_text)  # 调用 self.spark 方法并传递输入文本
+        current_text = self.plainTextEdit_2.toPlainText()
+        self.plainTextEdit_2.setPlainText(current_text + output_text + "\n")
+
 
 class MyDialog(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self):
